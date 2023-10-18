f7c837c730b4432138c5ac01b316059e12f532a6
ted111712 <ted111712@gmail.com>
2023-10-18 14:34:49 +0900

    Feat: 회원가입, 로그인, 질의응답 페이지 생성

--- hanwooplz_project/hanwooplz_app/views.py
+++ hanwooplz_project/hanwooplz_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    return render(request, "registration/register.html")
+
+def custom_login(request):
+    return render(request, "registration/login.html")
+
+def question_list(request):
+    return render(request, "question_list.html")
+
+def question_detail(request):
+    return render(request, "question.html")
+
+def create_question(request):
+    return render(request, "question_form.html")
