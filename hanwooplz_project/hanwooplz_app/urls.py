4f72df70dc07e817239efea3e182d3b46e2f6e04
giyong-choi <rldyd888@naver.com>
2023-10-18 15:15:23 +0900

    feat: write 페이지, chat 페이지 추가

--- hanwooplz_project/hanwooplz_app/urls.py
+++ hanwooplz_project/hanwooplz_app/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'hanwooplz_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('write/',views.write, name='write'),
+    path('chat/', views.current_chat, name='chat'),
 ]
--- hanwooplz_project/hanwooplz_app/views.py
+++ hanwooplz_project/hanwooplz_app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+# 게시글 작성 화면
+def write(request):
+    return render(request, 'write.html')
+    
+# 게시글 수정 화면
+# def edit(request, id):
+#     post = get_object_or_404(Post, id=id)
+#     if post:
+#         post.description = post.description.strip()
+#     if request.method == "POST":
+#         post.title = request.POST['title']
+#         post.description = request.POST['description']
+#         post.save()
+#         return redirect('hanwooplz_app:trade_post', pk=id)
+
+#     return render(request, 'write.html', {'post': post})
+
+def current_chat(request):
+    return render(request, 'chat.html')
