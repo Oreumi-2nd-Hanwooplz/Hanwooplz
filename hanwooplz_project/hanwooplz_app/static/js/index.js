f25a19aea36463a9dd36c3e7ce5451cfde622667
hwan3526 <hwan3526@gmail.com>
2023-10-22 23:06:39 +0900

    chore: modularize the css and js script in html

---
+++ hanwooplz_project/hanwooplz_app/static/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#list').click(function (event) { event.preventDefault(); $('#products .item').addClass('list-group-item'); });
+    $('#grid').click(function (event) { event.preventDefault(); $('#products .item').removeClass('list-group-item'); $('#products .item').addClass('grid-group-item'); });
+});
+async function generateResponse(input) {
+    const response = await fetch('/execute_chatbot/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': '{{ csrf_token }}',
+        },
+        body: JSON.stringify({ question: input }),
+    });
+
+    const data = await response.json();
+    return data;
+}
