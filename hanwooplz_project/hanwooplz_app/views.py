edf4710cdcc09b0ef78f6f20ba1f6631251242ab
ted111712 <ted111712@gmail.com>
2023-10-23 18:00:34 +0900

    feat: 댓글 기능 추가

--- hanwooplz_project/hanwooplz_app/views.py
+++ hanwooplz_project/hanwooplz_app/views.py
@@ -8,6 +8,11 @@ from django.http import HttpResponse
 from django.views.generic import FormView
 from . import forms, models
 from .forms import CustomUserCreationForm, LoginForm
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CommentSerializer
+from .models import comment
 
 # Create your views here.
 def index(request):
@@ -560,3 +565,41 @@ def chat_msg(request, room_number):
     }
 
     return render(request, 'chat.html', context)
+
+class CommentList(APIView):
+    def get(self, request):
+        comments = comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    
+    def post (self, request):
+        author = request.user if request.user.is_authenticated else None
+
+        comment_data = {
+            "content": request.data.get("content"),
+            "author": author,
+        }
+        serializer = CommentSerializer(data=comment_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CommentDetail(APIView):
+    def get(self, request, pk):
+        comment = get_object_or_404(comment, pk=pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        comment = get_object_or_404(comment, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        comment = get_object_or_404(comment, pk=pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- hanwooplz_project/hanwooplz_project/settings.py
+++ hanwooplz_project/hanwooplz_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hanwooplz_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
