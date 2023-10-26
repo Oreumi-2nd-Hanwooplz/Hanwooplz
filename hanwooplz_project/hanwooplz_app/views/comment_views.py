61cc262fa9c01d8f4a076112476d3621ee7be6e7
ted111712 <ted111712@gmail.com>
2023-10-26 17:48:10 +0900

    feat: 댓글 기능 구현
    - 게시물에 따라 조회, 추가되도록 수정
    - 좋아요, 댓글 삭제 기능 추가

--- hanwooplz_project/hanwooplz_app/views/comment_views.py
+++ hanwooplz_project/hanwooplz_app/views/comment_views.py
@@ -16,37 +16,37 @@ from rest_framework.response import Response
 from ..serializers import *
 
 class CommentList(APIView):
-    def get(self, request):
-        comments = Comment.objects.all()
+    def get(self, request, post_id):
+        comments = Comment.objects.filter(post=post_id).order_by("created_at")
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
     
-    def post (self, request):
+    def post (self, request, post_id):
         comment_data = {
             "content": request.data.get("content"),
         }
         serializer = CommentSerializer(data=comment_data)
         if serializer.is_valid():
-            serializer.save(author=self.request.user)
+            serializer.save(author=self.request.user, post_id=post_id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+  
 class CommentDetail(APIView):
-    def get(self, request, pk):
-        comment = get_object_or_404(comment, pk=pk)
+    def get(self, request, comment_id):
+        comment = get_object_or_404(Comment, pk=comment_id)
         serializer = CommentSerializer(comment)
         return Response(serializer.data)
     
-    def put(self, request, pk):
-        comment = get_object_or_404(comment, pk=pk)
+    def put(self, request, comment_id):
+        comment = get_object_or_404(Comment, pk=comment_id)
         serializer = CommentSerializer(comment, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self, request, pk):
-        comment = get_object_or_404(comment, pk=pk)
+    def delete(self, request, comment_id):
+        comment = get_object_or_404(Comment, pk=comment_id)
         comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
@@ -58,13 +58,17 @@ class CommentLikeView(generics.UpdateAPIView):
         comment = self.get_object()
         user = request.user
 
-        if user in comment.likes.all():
-            comment.likes.remove(user)
+        if user in comment.like.all():
+            comment.like.remove(user)
+            message = "좋아요가 취소되었습니다."
         else:
-            comment.likes.add(user)
+            comment.like.add(user)
+            message = ""
 
         comment.save()
-        return Response(self.get_serializer(comment).data)
+        comment_data = self.get_serializer(comment).data
+        context = {"comment_data": comment_data, "message": message}
+        return Response(context, status=status.HTTP_200_OK)
     
 # 댓글 테스트 용
 def question_test(request):
