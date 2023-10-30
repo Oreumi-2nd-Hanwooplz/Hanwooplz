042077a630736b5e9abbb429cb389321ce3f9304
hwan3526 <hwan3526@gmail.com>
2023-10-30 17:55:22 +0900

    feat: compose scripts to adapt aws with uwsgi and nginx

--- hanwooplz_project/hanwooplz_project/settings.py
+++ hanwooplz_project/hanwooplz_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
 
 # Application definition
