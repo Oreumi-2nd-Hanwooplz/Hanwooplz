28c74fccb34deea2b874898ab92b82856a1ea554
ted111712 <ted111712@gmail.com>
2023-11-02 15:49:20 +0900

    fix: 프로필 사진 불러오기 오류 수정

--- hanwooplz_project/hanwooplz_project/urls.py
+++ hanwooplz_project/hanwooplz_project/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hanwooplz_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.USERIMG_URL, document_root=settings.USERIMG_ROOT)
